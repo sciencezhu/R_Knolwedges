6366767231b4cd0956b55526e0b519bf76d7a927
sciencezhu <31102215+sciencezhu@users.noreply.github.com>
2022-01-23 14:26:10 -0700

    Update Filter_Within_Group_Data.Table.R

--- Filter_Within_Group_Data.Table.R
+++ Filter_Within_Group_Data.Table.R
@@ -3,6 +3,10 @@
 
 mtcars[, .SD[which.max(mpg)], by = cyl]
 
+mtcars[, .I[which.max(mpg)], by = cyl]    ## This approach is slightly faster per the above link's benchmark results.
+
+
+
 ## or
 
 mtcars[, .SD[get("mpg")==max(get("mpg")), by=c("cyl)]
